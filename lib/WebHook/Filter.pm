6a580fa012e2fea26adac5b9c9900b6337a4469e
Jakob Voss <voss@gbv.de>
2014-01-13 13:59:25 +0100

    renamed GitHub::WebHook to WebHook

---
+++ lib/WebHook/Filter.pm
@@ -0,0 +1,63 @@
+use strict;
+package WebHook::Filter;
+#ABSTRACT: Filter WebHook payload on common criteria
+#VERSION
+use v5.10;
+
+use parent 'WebHook';
+
+sub call {
+    my ($self, $payload) = @_;
+
+    foreach my $key (%$self) {
+
+        # prepare test routine (I miss the smartmatch operator!)
+        my $test = $self->{$key};
+
+        if (!ref $test) {
+            $self->{$key} = sub { $_[0] eq $test };
+        } elsif (ref $test eq 'Regexp') {
+            $self->{$key} = sub { $_[0] =~ $test };
+        } elsif (ref $test eq 'ARRAY') {
+            $self->{$key} = sub { scalar (grep { $_[0] eq $_ } @$test) };
+        }
+
+        my $value = $payload->{$key};
+
+        # repository_url, repository_owner_name, ...
+        my @parts = split /_/, $key;
+        shift @parts;
+        foreach (@parts) {
+            $value = eval { $value->{$_} } or return;
+        }
+
+        return unless $self->{$key}->($value);        
+    }
+    
+    return 1;
+}
+
+=head1 SYNOPSIS
+
+    my $app = Plack::App::WebHook->new(
+        hook => [
+            { 
+                Filter => {
+                    ref            => qr/master$/,
+                    repository_url => $url,
+                } 
+            },
+            sub { ... }; # only called if filter matches 
+        ];
+    );
+
+=head1 DESCRIPTION
+
+This L<WebHook> can be used to check whether a webhook payload matches some
+defined criteria.
+
+=encoding utf8
+
+=cut
+
+1;
---
+++ t/filter.t
@@ -0,0 +1,12 @@
+use strict;
+use Test::More;
+use WebHook;
+
+my $hook = WebHook->new('Filter', ref => qr/master$/);
+ok $hook->call({ ref => 'master' });
+ok !$hook->call({ });
+
+$hook = WebHook->new('Filter', ref => 'xxx', foo_bar => [qw(2 3)]);
+ok !$hook->call({ foo => { bar => 2 } , ref => 'xxx'});
+
+done_testing;
